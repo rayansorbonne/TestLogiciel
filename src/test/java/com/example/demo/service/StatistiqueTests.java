d023836e3a21498a79ece64e51723a1ce35b3c7a
rayansorbonne <rayan.kebbiche06@gmail.com>
2026-06-04 00:53:27 +0200

    tp2 test statistique

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -9,6 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StatistiqueTests {
 
-    
+    @Test
+    void prixMoyenSansVoiture(){
+        StatistiqueImpl statistique = new StatistiqueImpl();
+
+        assertThrows(ArithmeticException.class, () -> statistique.prixMoyen());
+    }
+
+    @Test
+    void prixMoyen(){
+        StatistiqueImpl statistique = new StatistiqueImpl();
+        statistique.ajouter(new Voiture("Renault", 10000));
+        statistique.ajouter(new Voiture("Peugeot", 20000));
+
+        Echantillon echantillon = statistique.prixMoyen();
+
+        assertEquals(2, echantillon.getNombreDeVoitures());
+        assertEquals(15000, echantillon.getPrixMoyen());
+    }
 
 }
