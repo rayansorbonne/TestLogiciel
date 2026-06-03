b4b5b0012d7fa0c138f62b7f84c42fefe93fda4d
rayansorbonne <rayan.kebbiche06@gmail.com>
2026-06-04 00:52:56 +0200

    first commit

---
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.data.Voiture;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class StatistiqueTests {
+
+    
+
+}
